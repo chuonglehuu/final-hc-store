97ae0582c935c6e203327e0176770cba7a7752d3
Brian Lee <lehuuchuong62@gmail.com>
2022-08-13 16:38:55 +0700

    Test branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>Đây là demo</h2>
+      <>
+        <h2>HC Store</h2>
+        <p>Đây là testing</p>
+      </>
     </div>
   );
 }
