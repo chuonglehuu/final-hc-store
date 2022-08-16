f2e659299d2130a6e52812f4c38a2a2d99bbd892
Brian Lee <lehuuchuong62@gmail.com>
2022-08-16 13:18:00 +0700

    install firebase and config app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { getDatabase, ref, child, get } from "firebase/database";
 import Login from "./components/Login";
 
 function App() {
+  const dbRef = ref(getDatabase());
+  const userId = 1;
+  get(child(dbRef, `users/${userId}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   return (
     <div className="App">
       <Login></Login>
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAb1EDY7RX36XBpUQIsOXq0Ff2N9lDhG5M",
+  authDomain: "hc-store-75829.firebaseapp.com",
+  projectId: "hc-store-75829",
+  storageBucket: "hc-store-75829.appspot.com",
+  messagingSenderId: "719242039093",
+  appId: "1:719242039093:web:e2c500655e26554898067c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
