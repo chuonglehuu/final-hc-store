5f1ca45d974cf84b51beda7891cb37acc4e3f271
Brian Lee <lehuuchuong62@gmail.com>
2022-08-17 17:05:35 +0700

    design default header

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,12 +1,34 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import images from "../../../../assets/Image";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx("wrapper")}>
-      <div className={cx("content")}></div>
+      <div className={cx("content")}>
+        <Link to="/">
+          <img className={cx("logo")} src={images.logo} alt="Logo" />
+        </Link>
+        <div className={cx("search")}>
+          <input placeholder="Search product or item" spellCheck={false} />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("actions")}></div>
+      </div>
     </header>
   );
 }
