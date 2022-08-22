ac13a4a8ce4b60a7afebe73b72fd10391518bbc3
Brian Lee <lehuuchuong62@gmail.com>
2022-08-22 17:35:06 +0700

    create footer

--- src/assets/Image/index.js
+++ src/assets/Image/index.js
@@ -1,4 +1,5 @@
 const images = {
   logo: require("./logo.png"),
+  logoFb: require("./logo-fb.png"),
 };
 export default images;
---
+++ src/components/Layout/DefaultLayout/Footer/ListContact/index.js
@@ -0,0 +1,8 @@
+const contactPhone = [{ phone: "0236.376.777" }, { phone: "0377.280.819" }];
+const contactMail = [
+  { mail: "hcstore@gmail.com" },
+  { mail: "lehuuchuong62@gmail.com" },
+  { mail: "chuonglhgbd19926@fpt.edu.vn" },
+];
+
+export { contactPhone, contactMail };
