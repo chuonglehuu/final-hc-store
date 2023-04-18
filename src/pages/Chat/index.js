b35f206bdc9b39e507c2f5b394f723fff6c5fc1d
Brian Lee <lehuuchuong62@gmail.com>
2023-04-18 13:58:42 +0700

    filter list conversation

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -6,7 +6,13 @@ import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
-import { collection, doc, getDocs, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { UserAuth } from "../../context/AuthContext";
 import { db } from "../../firebase/config";
@@ -21,10 +27,13 @@ function Chat() {
   const [conversations, setConversations] = useState([]);
   const [conversationSelected, setConversationSelected] = useState(null);
   const [messagesFormat, setMessagesFormat] = useState([]);
+  const [listChatWithoutAdmin, setListChatWithAdmin] = useState([]);
 
   const onSelect = (userId) => {
+    // khi select một conversation khác thì clean list messages hiện tại
     setMessages([]);
     if (conversations.length && role === 0) {
+      // convert sang string để so sánh dựa vào uid của user và uid của admin -> conversation
       const findConversation = conversations.find(
         (item) =>
           JSON.stringify(item.members) ===
@@ -35,7 +44,9 @@ function Chat() {
   };
 
   const handleFetchMessages = async () => {
+    // trường hợp là user: conversations chỉ có một phần tử chính là admin
     if (role !== 0) {
+      // get data của conversation
       const conversationRef = doc(db, "conversations", conversations[0].id);
       const messagesRef = collection(conversationRef, "messages");
       const querySnapshot = await getDocs(messagesRef);
@@ -44,11 +55,13 @@ function Chat() {
         tempMessages.push({ ...doc.data(), id: doc.id });
       });
 
+      // docChanges với change type action -> api của firebase check xem mỗi lần 
+      // data được thêm mới vào một collection -> cụ thể là thêm mới vào messages
       onSnapshot(messagesRef, (snapshot) => {
         snapshot.docChanges().forEach((change) => {
           if (change.type === "added") {
             const data = change.doc.data();
-            // let exists = messages.some((item) => item.id === data.id);
+            // khi nhận được data mới, thì add new data vào list messages
             if (data.type === "text") {
               setMessages((pre) => [...pre, data]);
             }
@@ -57,6 +70,8 @@ function Chat() {
       });
     }
 
+    // trường hợp là admin: list conversations là danh sách các cuộc trò chuyện
+    // đã có tin nhắn, khi nhắn tin phải dựa vào Id conversation selected
     if (role === 0) {
       const conversationRef = doc(db, "conversations", conversationSelected.id);
       const messagesRef = collection(conversationRef, "messages");
@@ -66,6 +81,7 @@ function Chat() {
         tempMessages.push({ ...doc.data(), id: doc.id });
       });
 
+      // api docChanges tương tự như ở trên
       onSnapshot(messagesRef, (snapshot) => {
         snapshot.docChanges().forEach((change) => {
           if (change.type === "added") {
@@ -93,16 +109,19 @@ function Chat() {
   };
 
   useEffect(() => {
+    // get list users
     onSnapshot(collection(db, "users"), (snapshot) => {
       const listUsers = snapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
       }));
+      // role = user -> thì chỉ lấy ra admin
       if (role === 2) {
         const filterAdmin = listUsers.filter((item) => item.role === 0);
         setUsersChat(filterAdmin);
       }
 
+      // role = admin -> thì lấy ra list user
       if (role === 0) {
         const filterUsers = listUsers.filter((item) => item.role === 2);
         setUsersChat(filterUsers);
@@ -115,6 +134,7 @@ function Chat() {
         id: doc.id,
       }));
 
+      // role = 2 -> conversation chỉ có một chính là user và admin
       if (role !== 0) {
         const filterConversationUserWithAdmin = listConversations.filter(
           (item) =>
@@ -124,7 +144,28 @@ function Chat() {
         setConversations(filterConversationUserWithAdmin);
       }
 
-      if (role === 0) setConversations(listConversations);
+      // role = 0 -> conversations là một mảng những conversation đã có tin nhắn
+      if (role === 0) {
+        // Lấy collection "messages" của mỗi conversation
+        listConversations.forEach((conversation) => {
+          const conversationRef = doc(db, "conversations", conversation.id);
+          const messagesQuery = query(collection(conversationRef, "messages"));
+          onSnapshot(messagesQuery, (messagesSnapshot) => {
+            const messages = messagesSnapshot.docs.map((doc) => ({
+              ...doc.data(),
+              id: doc.id,
+            }));
+            if (messages.length === 1) {
+              setListChatWithAdmin([
+                ...listChatWithoutAdmin,
+                conversation.members[0],
+              ]);
+            }
+            conversation.messages = messages;
+          });
+        });
+        setConversations(listConversations);
+      }
     });
   }, [role, userDetail]);
 
@@ -134,33 +175,68 @@ function Chat() {
 
   useEffect(() => {
     if (role !== 0) {
-      const newMessages = messages.map((item) => {
-        return {
-          ...item,
-          senderName:
-            item.senderId === userDetail.uid ? userDetail?.fullname : "ADMIN",
-        };
-      });
+      // foramt lại message -> thêm vào mỗi object message một key senderName -> render UI 
+      // sau đó là sort tin nhắn
+      const newMessages = messages
+        .map((item) => {
+          return {
+            ...item,
+            senderName:
+              item.senderId === userDetail.uid ? userDetail?.fullname : "ADMIN",
+          };
+        })
+        .sort((a, b) => {
+          const dateA = new Date(
+            a.timestamp.seconds * 1000 + a.timestamp.nanoseconds / 1000000
+          );
+          const dateB = new Date(
+            b.timestamp.seconds * 1000 + b.timestamp.nanoseconds / 1000000
+          );
+
+          return dateA - dateB;
+        });
       setMessagesFormat(newMessages);
     }
     if (role === 0) {
-      const newMessages = messages.map((item) => {
-        if (userDetail.uid === item.senderId) {
-          return {
-            ...item,
-            senderName: "ADMIN",
-          };
-        } else {
-          const findUser = users.find((user) => user.uid === item.senderId);
-          return {
-            ...item,
-            senderName: findUser?.fullname || "NULL",
-          };
-        }
-      });
+      const newMessages = messages
+        .map((item) => {
+          if (userDetail.uid === item.senderId) {
+            return {
+              ...item,
+              senderName: "ADMIN",
+            };
+          } else {
+            const findUser = users.find((user) => user.uid === item.senderId);
+            return {
+              ...item,
+              senderName: findUser?.fullname || "NULL",
+            };
+          }
+        })
+        .sort((a, b) => {
+          const dateA = new Date(
+            a.timestamp.seconds * 1000 + a.timestamp.nanoseconds / 1000000
+          );
+          const dateB = new Date(
+            b.timestamp.seconds * 1000 + b.timestamp.nanoseconds / 1000000
+          );
+
+          return dateA - dateB;
+        });
       setMessagesFormat(newMessages);
     }
   }, [role, userDetail, users, messages]);
+
+  useEffect(() => {
+    // logic lọc ra những user đã có tin nhắn với admin
+    const newUsers = users.reduce((arr, current) => {
+      if (!listChatWithoutAdmin.includes(current.uid)) {
+        arr.push(current);
+      }
+      return arr;
+    }, []);
+    setUsersChat(newUsers);
+  }, [listChatWithoutAdmin]);
 
   return (
     <Box sx={{ height: "92vh", flexGrow: 1, marginTop: "2px" }}>
